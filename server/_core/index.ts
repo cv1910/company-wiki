922c00079badcdebf33f412898433341940c1be7
cv1910 <cv@anitahass.com>
2026-01-28 11:26:28 +0100

    Register simple auth routes

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -4,6 +4,7 @@ import { createServer } from "http";
 import net from "net";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { registerOAuthRoutes } from "./oauth";
+import { registerSimpleAuthRoutes } from "./simple-auth";
 import { appRouter } from "../routers";
 import { createContext } from "./context";
 import { serveStatic, setupVite } from "./vite";
@@ -39,6 +40,7 @@ async function startServer() {
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
   // OAuth callback under /api/oauth/callback
   registerOAuthRoutes(app);
+  registerSimpleAuthRoutes(app);
 
   // File upload endpoint
   const upload = multer({
